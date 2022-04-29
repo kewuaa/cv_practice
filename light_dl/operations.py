762b1187399f303da40cc52ff9bc61523f82470d
kewuaa <2019021409015@std.uestc.edu.cn>
2022-04-29 09:44:33 +0800

    few change

--- light_dl/operations.py
+++ light_dl/operations.py
@@ -8,7 +8,7 @@ class NotRequireErr(MyErr): pass
 
 
 class GraphNode:
-    
+
     def __init__(self, tensor, *, requires_grad=False):
         self.tensor = np.asarray(tensor)
         self.requires_grad = requires_grad
@@ -21,10 +21,10 @@ class GraphNode:
         self.subnode_l = None
         self.subnode_r = None
         self.operation = 'None'
-        
+
     def __str__(self):
         return str(self.tensor)
-    
+
     def __repr__(self):
         return '\n'.join((repr(self.tensor),
                           'requires grad:',
@@ -36,15 +36,15 @@ class GraphNode:
                           'operation:',
                           self.operation,
                           ))
-    
+
     def __call__(self, i, j=None):
         if j is None:
             return self.tensor[i]
         else: return self.tensor[i, j]
-    
+
     def __pos__(self):
         return self
-        
+
     def __add__(self, other):
         if not isinstance(other, GraphNode):
             other = GraphNode(other)
@@ -60,10 +60,10 @@ class GraphNode:
         other.father = new_node
         new_node.operation = 'add'
         return new_node
-    
+
     def __radd__(self, other):
         return self.__add__(other)
-    
+
     def __iadd__(self, other):
         if isinstance(other, GraphNode):
             tensor = other.tensor
@@ -71,7 +71,7 @@ class GraphNode:
             tensor = np.asarray(other)
         self.tensor += tensor
         return self
-    
+
     def __sub__(self, other):
         if not isinstance(other, GraphNode):
             other = GraphNode(other)
@@ -87,7 +87,7 @@ class GraphNode:
         other.father = new_node
         new_node.operation = 'sub'
         return new_node
-    
+
     def __neg__(self):
         new_node = GraphNode(-self.tensor)
         new_node.requires_grad = self.requires_grad
@@ -97,7 +97,7 @@ class GraphNode:
         self.father = new_node
         new_node.operation = 'neg'
         return new_node
-    
+
     def __rsub__(self, other):
         if not isinstance(other, GraphNode):
             other = GraphNode(other)
@@ -113,7 +113,7 @@ class GraphNode:
         other.father = new_node
         new_node.operation = 'rsub'
         return new_node
-    
+
     def __isub__(self, other):
         if isinstance(other, GraphNode):
             tensor = other.tensor
@@ -121,7 +121,7 @@ class GraphNode:
             tensor = np.asarray(other)
         self.tensor -= tensor
         return self
-    
+
     def __mul__(self, other):
         if not isinstance(other, GraphNode):
             other = GraphNode(other)
@@ -137,10 +137,10 @@ class GraphNode:
         other.father = new_node
         new_node.operation = 'mul'
         return new_node
-    
+
     def __rmul__(self, other):
         return self.__mul__(other)
-    
+
     def matmul(self, other, *, order='pos'):
         if not isinstance(other, GraphNode):
             other = GraphNode(other)
@@ -168,7 +168,7 @@ class GraphNode:
         other.father = new_node
         new_node.operation = 'matmul'
         return new_node
-    
+
     def __truediv__(self, other):
         if not isinstance(other, GraphNode):
             other = GraphNode(other)
@@ -184,7 +184,7 @@ class GraphNode:
         other.father = new_node
         new_node.operation = 'truediv'
         return new_node
-    
+
     def __rtruediv__(self, other):
         if not isinstance(other, GraphNode):
             other = GraphNode(other)
@@ -200,8 +200,7 @@ class GraphNode:
         other.father = new_node
         new_node.operation = 'rtruediv'
         return new_node
-        
-    
+
     def __pow__(self, other):
         new_tensor = self.tensor**other
         new_node = GraphNode(new_tensor)
@@ -213,7 +212,7 @@ class GraphNode:
         self.father = new_node
         new_node.operation = 'pow'
         return new_node
-    
+
     def sum(self, axis=None):
         if axis is not None:
             ones = np.ones(self.tensor.shape[axis])
@@ -229,7 +228,7 @@ class GraphNode:
             self.father = new_node
             new_node.operation = 'sum'
             return new_node
-        
+
     @property
     def T(self):
         new_tensor = self.tensor.T.copy()
@@ -241,7 +240,7 @@ class GraphNode:
         self.father = new_node
         new_node.operation = 'T'
         return new_node
-    
+
     def relu(self):
         new_tensor = np.where(self.tensor > 0, self.tensor, 0)
         new_node = GraphNode(new_tensor)
@@ -252,7 +251,7 @@ class GraphNode:
         self.father = new_node
         new_node.operation = 'relu'
         return new_node
-    
+
     def backward(self, init_grad=None, is_scalar=True):
         if self.is_leaf or self.as_unique:
             self._grad += init_grad
@@ -264,7 +263,7 @@ class GraphNode:
                 else:
                     init_grad = np.ones_like(self.tensor)
             grads = self.gradfunc(init_grad, is_scalar,
-                left=self.subnode_l, right=self.subnode_r)
+                                  left=self.subnode_l, right=self.subnode_r)
             left_grad = next(grads)
             if self.subnode_l is not None:
                 if self.subnode_l.requires_grad:
@@ -275,7 +274,7 @@ class GraphNode:
                     self.subnode_r.backward(
                         init_grad=next(grads), is_scalar=is_scalar)
             del grads
-            
+
     @property
     def grad(self):
         if not self.requires_grad:
@@ -284,19 +283,19 @@ class GraphNode:
         if not self.save_grad:
             self.reset_grad()
         return grad
-    
+
     def detach(self):
         self.as_unique = not self.as_unique
-    
+
     def reset_grad(self):
         self._grad = 0.
         if self.father is not None:
             self.father.reset_grad()
-            
+
     @property
     def shape(self):
         return self.tensor.shape
-        
+
     def reshape(self, *size ,shape=()):
         if shape:
             new_tensor = self.tensor.reshape(shape)
@@ -304,6 +303,5 @@ class GraphNode:
             new_tensor = self.tensor.reshape(size)
         return GraphNode(
                     new_tensor, requires_grad=self.requires_grad)
-        
-        
-        
+
+
